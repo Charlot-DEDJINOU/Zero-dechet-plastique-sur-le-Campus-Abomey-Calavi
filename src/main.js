ea7c579218e73d204525f99094ec954893691f1b
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-22 23:26:15 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
