1d05da661ef8a970102285d1753009d05289ab6e
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-22 23:24:31 +0100

    Component for download

---
+++ src/components/untils.js
@@ -0,0 +1,26 @@
+import { toPng } from 'html-to-image'
+
+export const downloadImage = async () => {
+  const visuel = document.getElementById('visuel')
+
+  const visuelDownload = visuel
+  console.log(visuelDownload.style.zoom)
+  visuelDownload.style.zoom = 1
+
+  console.log(visuelDownload.style.zoom)
+
+  await toPng(visuelDownload, { quality: 1 })
+    .then(function (dataUrl) {
+      var link = document.createElement('a')
+      link.download = "Zéro déchet plastique sur le Campus d'Abomey-Calavi"
+      link.href = dataUrl
+      link.click()
+    })
+    .catch(function (error) {
+      console.error("Erreur lors du téléchargement de l'image :", error)
+    })
+    .finally(function () {
+      const largeurEcran = window.screen.width
+      if (largeurEcran <= 500) visuelDownload.style.zoom = 0.65
+    })
+}
